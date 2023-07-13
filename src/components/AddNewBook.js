ff25d0797274591d9865467c1eaafaa525470df1
moFuhidy <mo7ammed9290@gmail.com>
2023-07-13 17:16:33 +0300

    Fix the input padding

--- src/components/AddNewBook.js
+++ src/components/AddNewBook.js
@@ -7,8 +7,8 @@ function AddNewBook() {
           Add New Book
         </h2>
         <form className=" flex justify-between gap-9">
-          <input type="text" name="bookName" placeholder="Book title" className=" placeholder:pl-4 placeholder:text-gray-400 w-[42rem] border-gray-100 border-solid border-2" />
-          <input type="text" name="bookAuthor" placeholder="Author name" className=" placeholder:pl-4 placeholder:text-gray-400 w-[42rem] border-gray-100 border-solid border-2" />
+          <input type="text" name="bookName" placeholder="Book title" className=" placeholder:pl-4 placeholder:text-gray-400 w-[42rem] border-gray-100 border-solid border-2 pl-4" />
+          <input type="text" name="bookAuthor" placeholder="Author name" className=" w-64 placeholder:pl-4 placeholder:text-gray-400 border-gray-100 border-solid border-2 pl-4" value="hi" />
           <button type="submit" className=" uppercase bg-[#0290ff] text-white w-48 h-11 rounded-[3px] roboto font-bold text-sm">Add book</button>
         </form>
       </div>
