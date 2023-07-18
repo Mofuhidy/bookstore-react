d3a434da516bcac805f4e8a8eaa8b75ff9fb5f9d
moFuhidy <mo7ammed9290@gmail.com>
2023-07-18 13:06:20 +0300

    Add the the functions to make the HTTPS requests

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,27 +1,48 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const APPID = 'K9GpBrit39mBb31QaF12';
+const BASEURL = `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${APPID}/books/`;
+
+const fetchBooks = createAsyncThunk('books/fetchBooks', async (thunkAPI) => {
+  try {
+    const response = await axios.get(BASEURL);
+    return response.data;
+  } catch (error) {
+    thunkAPI.rejectWithValue(error);
+  }
+});
+
+const addBookByAPI = createAsyncThunk('books/addBook', async (book, thunkAPI) => {
+  console.log(book);
+  const newBook = {
+    item_id: book.item_id,
+    title: book.title,
+    author: book.author,
+    category: book.category,
+  };
+
+  try {
+    const addBook = await axios.post(BASEURL, newBook);
+    return { book, addBook };
+  } catch (error) {
+    thunkAPI.rejectWithValue(error);
+  }
+});
+
+const deleteBookByAPI = createAsyncThunk('books/deleteBook', async (book, thunkAPI) => {
+  try {
+    const deleteBook = await axios.delete(`${BASEURL}${book.item_id}`);
+    return { book, deleteBook };
+  } catch (error) {
+    thunkAPI.rejectWithValue(error);
+  }
+});
 
 const initcialState = {
-  books:
-[
-  {
-    item_id: 'item1',
-    title: 'The Great Gatsby',
-    author: 'John Smith',
-    category: 'Fiction',
-  },
-  {
-    item_id: 'item2',
-    title: 'Anna Karenina',
-    author: 'Leo Tolstoy',
-    category: 'Fiction',
-  },
-  {
-    item_id: 'item3',
-    title: 'The Selfish Gene',
-    author: 'Richard Dawkins',
-    category: 'Nonfiction',
-  },
-],
+  books: [],
+  loading: 'idle', // 'pending' | 'succeeded' | 'failed'
+  error: undefined,
 };
 
 export const booksSlice = createSlice({
@@ -36,6 +57,7 @@ export const booksSlice = createSlice({
       state.books = state.books.filter((book) => book.item_id !== itemId);
     },
   },
+
 });
 
 export const { addBook, removeBook } = booksSlice.actions;
