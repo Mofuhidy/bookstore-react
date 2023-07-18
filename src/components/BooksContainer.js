8a0925c5e99121ddc5772b0ed84f244fe3c70bbf
moFuhidy <mo7ammed9290@gmail.com>
2023-07-18 14:29:42 +0300

    Add loading status for books

--- src/components/BooksContainer.js
+++ src/components/BooksContainer.js
@@ -1,21 +1,40 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import Book from './Book';
 import { fetchBooks } from '../redux/books/booksSlice';
 
 function BookContainer() {
-  const { books } = useSelector((state) => state.books);
+  const { books, error, loading } = useSelector((state) => state.books);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchBooks());
   }, [dispatch]);
+  if (loading === 'pending') {
+    return (
+      <>
+        <main className=" h-[50vh] overflow-auto mt-9 px-24 pb-20">
+          <h3>Loading...</h3>
+        </main>
+      </>
+    );
+  }
+  if (error) {
+    return (
+      <>
+        <main className=" h-[50vh] overflow-auto mt-9 px-24 pb-20">
+          <h3>{error}</h3>
+        </main>
+      </>
+    );
+  }
 
   return (
     <main className=" h-[50vh] overflow-auto mt-9 px-24 pb-20">
       { books.map((book) => (
         <Book
-          key={book.item_id}
+          key={uuidv4()}
           books={book}
         />
       ))}
